c6560524ca3c0c791e6814e0f189684523fd68d0
oliviertech212 <oliviertech212@gmail.com>
2024-03-29 18:37:20 +0200

    style setup design homepage initialize component folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const HomePAge = () => {
-  return <div>HomePAge</div>;
+  return (
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover & Share</h1>
+        <br className="max-md:hidden" />
+
+        <span className=" orange_gradient text-center">AI-Powered Prompts</span>
+
+        <p className="desc border text-center">
+          promptopia is an ope-source AI prompting tool for modern world to
+          discover , create and share creative prompts
+        </p>
+        {/* feed */}
+      </section>
+    </>
+  );
 };
 
 export default HomePAge;
