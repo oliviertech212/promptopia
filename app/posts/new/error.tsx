66becb13960c4b2e4df42f2283770c2b61711aa2
oliviertech212 <oliviertech212@gmail.com>
2024-03-29 12:14:27 +0200

    initialize nextjs application structure project implement routing in nexjs and handle nested routing in nextjs

---
+++ app/posts/new/error.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const Error = ({ error, reset }: { error: any; reset: VoidFunction }) => {
+  useEffect(() => {
+    console.log("error", error);
+  }, [error]);
+  return (
+    <div>
+      <h2>Something went wrong</h2>
+
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+};
+
+export default Error;
