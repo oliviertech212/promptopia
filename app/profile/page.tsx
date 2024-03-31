965656b564f0aeda1311320c96393c08a326ddee
oliviertech212 <oliviertech212@gmail.com>
2024-03-31 15:46:15 +0200

    implement delete and update server api perform delete for one prompt and update posts state

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import Profile from "@/components/Profile";
+import { Prompt } from "@/components/Feed";
 
 const ProfilePage = () => {
   const { data: session } = useSession();
@@ -12,8 +13,19 @@ const ProfilePage = () => {
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([]);
 
-  const handleEdit = async () => {};
-  const handleDelete = async () => {};
+  const handleEdit = async (post: Prompt) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post: Prompt) => {
+    const response = await fetch(`/api/prompt/${post._id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log("data", data);
+      setPosts(posts.filter((p: Prompt) => p._id !== post._id));
+    }
+  };
 
   useEffect(() => {
     // Fetch all prompts
@@ -23,7 +35,6 @@ const ProfilePage = () => {
       // console.log("prompts", data);
       setPosts(data);
     };
-
     if (session?.user?.id) fetchprompts();
   }, []);
 
